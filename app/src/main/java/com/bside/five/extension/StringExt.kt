202a2de3730c3a1314255cf7b7d234a45cf8af2e
경창현 <cg072@naver.com>
2021-06-17 14:45:49 +0900

    Warning 문제 수정
    1. Warning 문제 수정

--- app/src/main/java/com/bside/five/extension/StringExt.kt
+++ app/src/main/java/com/bside/five/extension/StringExt.kt
@@ -5,14 +5,14 @@ import java.util.regex.Pattern
 /**
  *  한글 구분
  */
-fun CharSequence?.isHangul(): Boolean = Pattern.matches("^[ㄱ-ㅎ가-힣0-9]*\$", this)
+fun CharSequence.isHangul(): Boolean = Pattern.matches("^[ㄱ-ㅎ가-힣0-9]*\$", this)
 
 /**
  *  영문 구분
  */
-fun CharSequence?.isEnglish(): Boolean = Pattern.matches("^[a-zA-Z\\s]+\$", this)
+fun CharSequence.isEnglish(): Boolean = Pattern.matches("^[a-zA-Z\\s]+\$", this)
 
 /**
  *  숫자 구분
  */
-fun CharSequence?.isNumeric(): Boolean = Pattern.matches("-?\\d+(\\.\\d+)?", this)
+fun CharSequence.isNumeric(): Boolean = Pattern.matches("-?\\d+(\\.\\d+)?", this)
