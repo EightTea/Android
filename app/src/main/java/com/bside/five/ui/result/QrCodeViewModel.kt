a362934767fa703181e1a1312d7d8a27090327a9
경창현 <cg072@naver.com>
2021-12-08 15:24:10 +0900

    Qr생성화면 종속성 분리 작업

--- app/src/main/java/com/bside/five/ui/result/QrCodeViewModel.kt
+++ app/src/main/java/com/bside/five/ui/result/QrCodeViewModel.kt
@@ -1,27 +1,29 @@
 package com.bside.five.ui.result
 
-import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.bside.five.R
 import com.bside.five.base.BaseViewModel
 import com.bside.five.util.ImageUtil
 
 class QrCodeViewModel : BaseViewModel() {
 
-    var imageSaveLive: MutableLiveData<String> = MutableLiveData()
-    val qrDrawable = ObservableField<Drawable>()
+    private val _imageSaveLive: MutableLiveData<String> = MutableLiveData()
+    val imageSaveLive: LiveData<String> get() = _imageSaveLive
     val isDownloadQr = ObservableField<Boolean>(false)
+    private val _isDownloadQrLive: MutableLiveData<Boolean> = MutableLiveData()
+    val isDownloadQrLive: LiveData<Boolean> get() = _isDownloadQrLive
 
-    override fun onClickListener(view: View) {
-        when (view.id) {
-            R.id.qrCodeSaveContainer -> {
-                isDownloadQr.set(true)
-                val fileName = System.currentTimeMillis().toString() + "_${ImageUtil.TEMP_FILE_NAME}"
-                imageSaveLive.postValue(fileName)
-            }
-        }
+    override fun onClickListener(view: View) {}
+
+    fun saveQr() {
+        _isDownloadQrLive.value = true
+        val fileName = System.currentTimeMillis().toString() + "_${ImageUtil.TEMP_FILE_NAME}"
+        _imageSaveLive.value = fileName
     }
 
+    fun finishDownloadQr() {
+        _isDownloadQrLive.value = false
+    }
 }
