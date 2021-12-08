7f1bebd95cb35822c5328ff31c45d615b4bd7df1
경창현 <cg072@naver.com>
2021-12-08 14:46:03 +0900

    설정화면 종속성 분리 작업

--- app/src/main/java/com/bside/five/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/bside/five/ui/setting/SettingActivity.kt
@@ -1,19 +1,23 @@
 package com.bside.five.ui.setting
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.bside.five.BuildConfig
 import com.bside.five.R
 import com.bside.five.base.BaseActivity
+import com.bside.five.constants.Constants
 import com.bside.five.custom.dialog.MoreBottomSheetDialog
 import com.bside.five.databinding.ActivitySettingBinding
+import com.bside.five.util.ActivityUtil
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class SettingActivity : BaseActivity<ActivitySettingBinding, SettingViewModel>() {
-
-    private val tag = this::class.java
 
     override val layoutResourceId: Int
         get() = R.layout.activity_setting
@@ -24,7 +28,8 @@ class SettingActivity : BaseActivity<ActivitySettingBinding, SettingViewModel>()
         super.onCreate(savedInstanceState)
 
         initToolbar()
-        viewModel.version.set(getString(R.string.setting_version, BuildConfig.VERSION_NAME))
+        listener()
+        viewModel.setVersion(getString(R.string.setting_version, BuildConfig.VERSION_NAME))
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -53,6 +58,37 @@ class SettingActivity : BaseActivity<ActivitySettingBinding, SettingViewModel>()
             setDisplayShowCustomEnabled(true)
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    private fun listener() {
+        binding.settingSupportBtn.setOnClickListener {
+            Toast.makeText(this, "준비중입니다.", Toast.LENGTH_LONG).show()
+        }
+        binding.settingContactBtn.setOnClickListener {
+            showContactEmail()
+        }
+        binding.settingPrivacyPolicyBtn.setOnClickListener {
+            ActivityUtil.startPrivacyPolicy(this)
+        }
+        binding.settingVersionBtn.setOnClickListener {
+            Toast.makeText(this, BuildConfig.VERSION_CODE.toString(), Toast.LENGTH_LONG).show()
+        }
+        binding.settingLicenseBtn.setOnClickListener {
+            ContextCompat.startActivity(this, Intent(this, OssLicensesMenuActivity::class.java), null)
+        }
+    }
+
+    private fun showContactEmail() {
+        val title = "알고싶어 - "
+        val contents = "App Version : ${BuildConfig.VERSION_NAME}\nOS Version : ${Build.VERSION.RELEASE}\n\n"
+
+        Intent(Intent.ACTION_SEND).let {
+            it.putExtra(Intent.EXTRA_EMAIL, arrayOf(Constants.EMAIL_ADDRESS))
+            it.putExtra(Intent.EXTRA_SUBJECT, title)
+            it.putExtra(Intent.EXTRA_TEXT, contents)
+            it.type = "message/rfc822"
+            startActivity(Intent.createChooser(it, "Choose Email"))
         }
     }
 }
